f058524fbc5dd29d46b89a93551d246cc0267e2f
jabez-abija2399 <jabeznew2399@gmail.com>
2026-04-19 21:29:01 +0300

    feat: integrate TalentForm and useTalentProfile hook into App component for form management

--- talent-profile/src/App.tsx
+++ talent-profile/src/App.tsx
@@ -1,11 +1,36 @@
+import { TalentForm } from "./components/profile/TalentForm";
+import { UseTalentProfile } from "./hook/useTalentProfile";
 
 
 function App() {
 
+  const {profile, handleChange, handleSubmit, isFormValid, view} = UseTalentProfile();
+
   return (
-    <h1 className='text-5xl font-bold text-indigo-600 underline'>
-  Tailwind is Working!
-</h1>
+    <main>
+
+      {/* conditional rendering */}
+      {view === "FORM" ? (
+        <TalentForm
+        profile={profile}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+        isValid={isFormValid}
+        />
+      ):(
+        <div>
+          <h1>Success!</h1>
+          <p>Form logic is working/ Now we just need the profile car</p>
+
+          <button
+          onClick={() => window.location.reload()}
+          className="mt-6 text-primary font-medium hover:underline"
+          >
+            Reset Form
+          </button>
+        </div>
+      )}
+    </main>
 
   )
 }

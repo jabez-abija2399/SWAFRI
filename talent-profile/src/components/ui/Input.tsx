dccd72783d5f3fa57737f150ce45d46e75627f09
jabez-abija2399 <jabeznew2399@gmail.com>
2026-04-19 20:56:05 +0300

    add multiline support to Input component to allow rendering as a textarea

--- talent-profile/src/components/ui/Input.tsx
+++ talent-profile/src/components/ui/Input.tsx
@@ -1,12 +1,16 @@
-import type { InputHTMLAttributes } from "react";
+import type { InputHTMLAttributes, TextareaHTMLAttributes } from "react";
 import { cn } from "../../utils/cn";
 
-
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface BaseProps {
     label: string,
     error?: string,
     containerClassName?: string,
+    multiline?: boolean,
 }
+
+type TextFieldProps = BaseProps & 
+InputHTMLAttributes<HTMLInputElement> &
+TextareaHTMLAttributes<HTMLTextAreaElement>
 
 export const Input = ({
     label,
@@ -14,8 +18,18 @@ export const Input = ({
     className,
     containerClassName,
     id,
+    multiline = false,
     ...props
-}: InputProps) => {
+}: TextFieldProps) => {
+
+    const sharedClasses = cn(
+    "px-4 py-2 bg-white border border-secondary-border rounded-lg outline-none w-full",
+    "transition-all duration-200 placeholder:text-text-muted text-text-main",
+    "focus:border-primary focus:ring-2 focus:ring-primary/10",
+    error && "border-red-500 focus:border-red-500 focus:ring-red-500/10",
+    className
+  );
+
     return (
         <div className={cn("flex flex-col gap-1.5 w-full",
             containerClassName
@@ -28,18 +42,20 @@ export const Input = ({
                 {label}
             </label>
 
-            <input
+            {multiline ? (
+                <textarea
                 id={id}
                 className={cn(
-                    "px-4 py-2 bg-white border border-secondary-border rounded-lg outline-none",
-                    "transition-all duration-200 placeholder:text-test-muted text-text-main",
-                    "focus:border-primary focus:ring-2 focus:ring-primary/10",
-
-                    error && "border-red-500 focus:border-red-500 focus:ring-red-500/10",
-                    className
-                )}
-                {...props}
+                    "min-h-[100px] resize-y", sharedClasses)}
+                    {...(props as TextareaHTMLAttributes<HTMLTextAreaElement>)}
+                />
+            ):(
+            <input
+                id={id}
+                className={sharedClasses}
+                {...(props as InputHTMLAttributes<HTMLInputElement>)}
             />
+            )}   
 
             {error && (
                 <p className="text-sm text-red-500 font-medium">{error}</p>

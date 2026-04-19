2ad632bb4b1f5b0d91806e3c04d42da6f6cbddb6
jabez-abija2399 <jabeznew2399@gmail.com>
2026-04-19 20:09:44 +0300

    implement useTalentProfile hook for state management and form handling

---
+++ talent-profile/src/hook/useTalentProfile.ts
@@ -0,0 +1,44 @@
+import { useState, type ChangeEvent } from "react";
+import type { AppView, TalentProfile } from "../types";
+
+
+export const UseTalentProfile = () => {
+    const [profile, setProfile] = useState<TalentProfile>({
+        fullName: "",
+        email: "",
+        primarySkill: "",
+        yearsOfExp: 0,
+        description: "",
+    });
+
+    const [view, setView] = useState<AppView>("FORM")
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+
+        setProfile((prev) => ({
+            ...prev,
+            [name]: name == 'yearsOfExp' ? Number(value) : value,
+        }))
+    };
+
+    const isSenior = profile.yearsOfExp >= 5;
+    const isFormValid = profile.fullName && profile.email && profile.primarySkill;
+
+    const handleSubmit = () => {
+        if (isFormValid) setView("PROFILE")
+    }
+
+    const handleEdit = () => setView("FORM")
+
+
+    return {
+        profile,
+        view,
+        handleChange,
+        handleSubmit,
+        handleEdit,
+        isSenior,
+        isFormValid,
+    }
+}
